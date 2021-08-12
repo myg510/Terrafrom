274749e29b3f8a26388aceac6710d82ae8caa30e
zonameng <zonameng@163.com>
2021-08-12 12:35:35 +0800

    Changed some files

--- terrafrom-upload/main.tf
+++ terrafrom-upload/main.tf
@@ -2,7 +2,7 @@ data "vsphere_datacenter" "dc" {
   name = var.pro_datacenter
 }
 
-resource "vsphere_file" "ubuntu_disk_upload" {
+resource "vsphere_file" "upload_file" {
   datacenter       = "sunshineit"
   datastore        = "iscsi_lun01"
   source_file      = "/Users/mengyinggang/Downloads/VMware-ESXi-6.7.0.update03-17700523-LNV-20210408.iso"
